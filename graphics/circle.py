352a7c4e756b61b3815bb01053e2876c04a7da34
Vipin Kumar N <binduvipinkumar39@gmail.com>
2022-03-06 00:32:32 +0530

    added Graphics Package

---
+++ graphics/circle.py
@@ -0,0 +1,4 @@
+def circlearea(radius):
+    return 3.14*(radius*radius)
+def circleperi(radius):
+    return 2*3.14*radius
---
+++ graphics/rectangle.py
@@ -0,0 +1,4 @@
+def rectarea(length,breadth):
+    return length*breadth
+def rectperi(length,breadth):
+    return 2*(length+breadth)
