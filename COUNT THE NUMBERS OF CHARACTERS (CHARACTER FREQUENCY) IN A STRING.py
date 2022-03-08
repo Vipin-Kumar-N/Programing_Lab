33ff4c017cd58b2d7837902de72708fae0070cbe
Vipin Kumar N <binduvipinkumar39@gmail.com>
2022-03-08 15:17:30 +0530

    edited count

--- COUNT THE NUMBERS OF CHARACTERS (CHARACTER FREQUENCY) IN A STRING.py
+++ COUNT THE NUMBERS OF CHARACTERS (CHARACTER FREQUENCY) IN A STRING.py
@@ -6,3 +6,5 @@ for i in test1:
     else:
         freq[i]=1
 print("Count of all Characters in "+test1+"is : \n"+str(freq))
+str1=input("Enter the substring to found : ")
+print("No of occurences of substring is ",test1.count(str1))
